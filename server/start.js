2460161b677a9fbb68c991cb4eaec2cec5aca2d0
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-10-08 21:08:39 -0400

    updates for prod\

--- server/start.js
+++ server/start.js
@@ -5,17 +5,17 @@ const {resolve} = require('path');
 const pkg = require('../package.json');
 const app = express();
 
+const env = process.env;
+const cPort = env.PORT || 3000;
+
 module.exports = app
   .use(express.static(resolve(__dirname, '..', 'public')))
   .use('/api', require('./api'))
   .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')));
 
-if (module === require.main) {
-  const server = app.listen(
-    process.env.PORT || 3000,
-    () => {
-      console.log(`--- Started HTTP Server for ${pkg.name} ---`);
-      console.log(`Listening on ${JSON.stringify(server.address())}`);
-    }
-  );
-}
+const server = app.listen(cPort, () => {
+  const { address, port } = server.address();
+  const host = address === '::' ? 'localhost' : address;
+  const urlSafeHost = host.includes(':') ? `[${host}]` : host;
+  console.log(`${pkg.name} running at: http://${urlSafeHost}:${port}`);
+});
