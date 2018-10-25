62ac28ade9b5e0ede132e207035767a4ecd00bb5
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-10-24 23:53:55 -0400

    Add to error message

--- app/components/Error.jsx
+++ app/components/Error.jsx
@@ -1,9 +1,12 @@
 'use strict';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import styles from '../styles/game';
 
 const Error = (props) => {
-  const { status, summoner, title } = props;
+  const { classes, status, summoner, title } = props;
   let errMessage = `Error loading games for ${summoner}`;
   if (status === 404) errMessage = `Invalid summoner name ${summoner}`;
     return (
@@ -13,8 +16,11 @@ const Error = (props) => {
           zDepth={4} >
           <h3 style={title}>{errMessage}</h3>
         </Paper>
+        <Paper className={ classes.root } >
+          <Typography>Please try your search again.</Typography>
+        </Paper>
       </div>
     );
 };
 
-export default Error;
+export default withStyles(styles)(Error);
